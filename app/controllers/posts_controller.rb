2041f1372f92fffc9970cc93554b3185eac9f2c7
bazz666 <herrera.se1992@gmail.com>
2021-05-14 16:10:40 -0400

    add validations and solve some errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
-  def dashboard
-  end
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
 
   def index
+    @posts = Post.order(created_at: :desc)
   end
 
   def create
+    @post = Post.create(title: params[:title], image_url: params[:image_url], content: params[:content])
+  end
+
+  def dashboard
   end
 end
